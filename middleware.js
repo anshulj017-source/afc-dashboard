443b757fd352300828ed664ef7f63abd2743c761
anshulj017-source <anshulj017@gmail.com>
2026-06-11 00:26:04 +0400

    Update route matcher to protect all routes except sign-in and sign-up

--- middleware.js
+++ middleware.js
@@ -1,11 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
+// This protects all routes EXCEPT the sign-in and sign-up pages
 const isProtectedRoute = createRouteMatcher([
-  '/(.*)'
+  '/((?!sign-in|sign-up).*)'
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  if (isProtectedRoute(req)) {
+    auth().protect();
+  }
 });
 
 export const config = {
